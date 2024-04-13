74e8984781cd628cacb1d2b62e758aa8b0554324
jSdCool <duffysd@sunypoly.edu>
2024-04-13 16:16:10 -0400

    pile counter

--- prob2/src/comp prob 2.cpp
+++ prob2/src/comp prob 2.cpp
@@ -9,6 +9,52 @@ double calculateScore(int yardSize,int acornCount, int pileCount,int opperationC
 	return result;
 }
 
+void discoverPile(char yard[],int size,int i,int j){
+	//set the current space to not pile
+	yard[i*size+j] = '.';
+	//check to space above
+	if(i-1 >0 && yard[(i-1)*size+j] >= '1' && yard[(i-1)*size+j] <= '9'){
+		discoverPile(yard,size,i-1,j);
+	}
+	//check the space to the left
+	if(j-1 >0 && yard[i*size+j-1] >= '1' && yard[(i-1)*size+j] <= '9'){
+		discoverPile(yard,size,i,j-1);
+	}
+	//check the space to the right
+	if(i+1 <size && yard[(i+1)*size+j] >= '1' && yard[(i-1)*size+j] <= '9'){
+		discoverPile(yard,size,i+1,j);
+	}
+	//check the space down
+	if(j+1 <size && yard[i*size+j+1] >= '1' && yard[(i-1)*size+j] <= '9'){
+		discoverPile(yard,size,i,j+1);
+	}
+
+}
+
+int countPiles(int yardSize, char yard[]){
+	char yardCopy[yardSize][yardSize];
+	//copy the yard
+	for(int i=0;i<yardSize;i++){
+		for(int j=0;j<yardSize;j++){
+			yardCopy[i][j] = yard[i*yardSize+j];
+			discoverPile(yardCopy[0],yardSize,i,j);
+		}
+	}
+	int piles =0;
+	//loop through the yard to find piles
+	for(int i=0;i<yardSize;i++){
+		for(int j=0;j<yardSize;j++){
+			//if the current selection is a pile
+			if(yardCopy[i][j]>='1' && yardCopy[i][j]<='9'){
+				piles++;
+			}
+		}
+	}
+
+	return piles;
+}
+
+
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
