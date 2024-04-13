e64c729b75bce7dfc22af5f17ed90fa7cc1b0dc5
jSdCool <duffysd@sunypoly.edu>
2024-04-13 17:24:08 -0400

    mask center pile

--- prob2/src/comp prob 2.cpp
+++ prob2/src/comp prob 2.cpp
@@ -18,24 +18,24 @@ double calculateScore(int yardSize,int acornCount, int pileCount,int opperationC
 	return result;
 }
 
-void discoverPile(char yard[],int size,int i,int j){
+void discoverPile(char yard[],int size,int i,int j,char filler){
 	//set the current space to not pile
-	yard[i*size+j] = '.';
+	yard[i*size+j] = filler;
 	//check to space above
 	if(i-1 >=0 && yard[(i-1)*size+j] >= '1' && yard[(i-1)*size+j] <= '9'){
-		discoverPile(yard,size,i-1,j);
+		discoverPile(yard,size,i-1,j,filler);
 	}
 	//check the space to the left
 	if(j-1 >=0 && yard[i*size+j-1] >= '1' && yard[i*size+j-1] <= '9'){
-		discoverPile(yard,size,i,j-1);
+		discoverPile(yard,size,i,j-1,filler);
 	}
 	//check the space to the right
 	if(i+1 <size && yard[(i+1)*size+j] >= '1' && yard[(i+1)*size+j] <= '9'){
-		discoverPile(yard,size,i+1,j);
+		discoverPile(yard,size,i+1,j,filler);
 	}
 	//check the space down
 	if(j+1 <size && yard[i*size+j+1] >= '1' && yard[(i)*size+j+1] <= '9'){
-		discoverPile(yard,size,i,j+1);
+		discoverPile(yard,size,i,j+1,filler);
 	}
 
 }
@@ -56,7 +56,7 @@ int countPiles(int yardSize, char yard[]){
 			//if the current selection is a pile
 			if(yardCopy[i][j]>='1' && yardCopy[i][j]<='9'){
 				piles++;
-				discoverPile(yardCopy[0],yardSize,i,j);
+				discoverPile(yardCopy[0],yardSize,i,j,'.');
 			}
 		}
 	}
@@ -120,6 +120,21 @@ void drop(game &gameState){
 			gameState.holding=false;
 		}
 	}
+}
+
+void generateCneterPileMask(game gameState,char outputmask[]){
+	//copy the yard
+	for(int i=0;i<gameState.yardSize;i++){
+		for(int j=0;j<gameState.yardSize;j++){
+			outputmask[i*gameState.yardSize+j] = gameState.yard[i*gameState.yardSize+j];
+		}
+	}
+	//check if a part of a pile exsists at the center pos
+	if(outputmask[gameState.yardSize/2*gameState.yardSize+gameState.yardSize/2] >='1' && outputmask[gameState.yardSize/2*gameState.yardSize+gameState.yardSize/2] <='9' ){
+		//mask out the center pile using x
+		discoverPile(outputmask,gameState.yardSize,gameState.yardSize/2,gameState.yardSize/2,'x');
+	}
+
 }
 
 
