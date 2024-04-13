b741e3faa41f9fdf4e70b786f8cd9915e799af05
jSdCool <duffysd@sunypoly.edu>
2024-04-13 15:45:24 -0400

    remove header comments

--- prob1/src/comp problem 1.cpp
+++ prob1/src/comp problem 1.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-// Name        : comp.cpp
-// Author      : jSdCool
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 using namespace std;
 
--- prob2/src/comp prob 2.cpp
+++ prob2/src/comp prob 2.cpp
@@ -1,13 +1,5 @@
-//============================================================================
-// Name        : comp.cpp
-// Author      : jSdCool
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
 
-#include <iostream>
-using namespace std;
+#include <iostream>using namespace std;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
